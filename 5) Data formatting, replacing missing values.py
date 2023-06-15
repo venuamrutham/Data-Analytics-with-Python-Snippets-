d1c4e5728a3b72ee5ea70ea2fda8151e13726a56
Amrutham venu <115334303+venuamrutham@users.noreply.github.com>
2023-06-15 10:11:52 +0530

    Add files via upload

---
+++ 5) Data formatting, replacing missing values.py
@@ -0,0 +1,41 @@
+# Data formatting, replacing missing values, coverting
+
+# import necessary libraries
+import pandas as pd
+import tkinter as tk
+from tkinter import filedialog
+import numpy as np
+
+# Create a root
+root = tk.Tk()
+root.withdraw()
+
+# Open filepicker dialogue
+file_path = filedialog.askopenfilename()
+
+if file_path:
+    # Load data using pandas
+    df = pd.read_excel(file_path, header=None)
+
+    # Get the headers
+    headers = df.iloc[0].tolist()
+    df = df[1:]
+    df.columns = headers
+
+print(df)
+
+df['marks'] = pd.to_numeric(df['marks'], errors='coerce')     # Coverts the data into int type
+
+mean = df["marks"].mean()                                     # Finding mean
+
+df["marks"].replace(np.nan,mean,inplace=True)                 # Filling mean in Nan place, df["marks"].fillna(mean, inplace=True) also gives same result
+
+df.rename(columns = {"marks":"Updated_marks"}, inplace=True)  # Renaming the column
+
+print(df)
+
+
+
+
+
+
